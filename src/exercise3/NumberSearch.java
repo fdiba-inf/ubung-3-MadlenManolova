dbfccb16a467f880d4eaa454710a4f818613277c
MadlenManolova <madllymanolova@gmail.com>
2020-10-15 14:05:34 +0000

    NumberSearch

--- src/exercise3/NumberSearch.java
+++ src/exercise3/NumberSearch.java
@@ -1,15 +1,24 @@
 package exercise3;
 
-import java.util.Scanner;
 public class NumberSearch {
+
  public static void main (String[] args) {
 
-   Scanner input = new Scanner (System.in);
-   int count = 0;
+  int count = 0;
+   
    for (int i = 100; i <= 1000; i++) {
-     if (i/5 && i/6) {
-       System.out.print(i);
+     if (i % 5 == 0 && i % 6 == 0) {
+
+       count++;
+
+       if (count == 10){
+         count=0;
+         System.out.print(i);
+         System.out.println();
+       }else {
+         System.out.print(i + " ");
      }
    }
  }
+}
 }
