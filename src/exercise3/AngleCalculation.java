b52d9263b66b2465c292ad9a853fdaaee070e472
MadlenManolova <madllymanolova@gmail.com>
2020-10-17 16:31:52 +0000

    AngleCalculator

--- src/exercise3/AngleCalculation.java
+++ src/exercise3/AngleCalculation.java
@@ -6,6 +6,29 @@ public class AngleCalculation{
 
   public static void main (String[] args){
 
-   int a = input.nextInt();
+   Scanner input = new Scanner(System.in);
+
+        float a  = input.nextFloat();
+        
+        char einheit = input.next().charAt(0);
+
+        do{
+          if (einheit == 'r'){
+            double grad = a*180.0/Math.PI;
+            System.out.println("Angle: " + grad + "d");
+
+          } else {
+            double radiant = a*Math.PI/180.0;
+            System.out.println("Angle: " + radiant + "r");
+
+            
+          } 
+          a  = input.nextFloat();
+          einheit = input.next().charAt(0);
+        
+        }
+         
+        while (einheit == 'd' || einheit == 'r');
+        
   }
 }
